b681e4c3eaf0e713c939e84207cea892b8911645
AETSRound <qkrcksdna@naver.com>
2020-11-30 17:49:51 +0900

    첫째날 react 수업 끝

---
+++ src/TodoRow.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import styled from "styled-components"
+
+const TodoRow = ({index, todo, handleClickRemove}) => {
+    return (
+        <Container onClick={() => handleClickRemove(index)}>
+            {index}----{todo}
+        </Container>
+    )
+}
+
+const Container= styled.div``;
+
+export default TodoRow
